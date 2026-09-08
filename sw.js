101a87df89da0c13dabc3090da023840ed304ec5
fullmodel-star <fullmodel-star@users.noreply.github.com>
2026-09-08 06:57:10 +0000

    deploy 905_cap-english from monorepo 8cbcd0487a3cfffbc8a0a0204e3006a75c3d86d6

---
+++ sw.js
@@ -0,0 +1,6 @@
+const C = "905_cap-english-icons-8cbcd0487a";
+const A = ["./", "./index.html", "./data.js", "./rv2-core.js", "./practice.js", "./manifest.json", "./icon.svg", "./ridgeline-ui.css", "./favicon.svg", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./apple-touch-icon.png"];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+self.addEventListener('message',e=>{if(e.data==='SKIP_WAITING'||e.data==='skipWaiting'||(e.data&&e.data.type==='SKIP_WAITING'))self.skipWaiting()});
